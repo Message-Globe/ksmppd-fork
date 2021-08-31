ed03bccedbd000c04a4b2abbb4e659e9e540f7d9
tiltroom <you@example.com>
2021-08-31 21:48:46 +0200

    MO query

---
+++ database-schemas/MO.sql
@@ -0,0 +1,1 @@
+INSERT INTO `smpp_store` VALUES (NULL,'1233','+393427330094','','Hello world',-1,'lp1',NULL,'14','otptest','','c85d2a8e-96c0-494a-b94e-0f97f146d1ea',0,-1,-1,-1,-1,-1,-1,-1,NULL,-1,-1,-1,'',NULL,'',-1,-1,-1,-1,'');
